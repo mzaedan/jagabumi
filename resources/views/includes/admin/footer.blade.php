ad8bd32eee9e121eeb204eff990ee24e8d9353de
mzaedan <zaedanm108@gmail.com>
2024-06-03 14:14:10 +0700

    update

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="https://smkwidyadirgantara.sch.id/">SMK WIDYA DIRGANTARA</a> </strong>
+    <strong>Copyright &copy; 2023 <a href="#">JAGA BUMI</a> </strong>
     All rights reserved.
 </footer>
