60eb1c3318dcfca9786e8c3ba4b31fb8156ea804
mzaedan <zaedanm108@gmail.com>
2024-06-04 21:20:17 +0700

    update

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -7,8 +7,8 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="{{ route('beranda') }}" class="nav-link">Home</a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
+      {{-- <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
-      </li>
+      </li> --}}
     </ul>
 </nav>
