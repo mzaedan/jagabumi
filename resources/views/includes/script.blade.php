6493ae7191fecc020f09e002dbd6681f962b69b6
SyafaatAkbar123 <syafaatakbar123@gmail.com>
2024-06-04 22:47:07 +0700

    fixing hamburger button + card + image responsive

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,2 +1,4 @@
 <script src="https://kit.fontawesome.com/6132c4afda.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="{{ url('js/script.js')}}"></script>
