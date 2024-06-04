3afd89f7e3e4d867c7f7e500488063f0aee07f22
mzaedan <zaedanm108@gmail.com>
2024-06-04 21:15:13 +0700

    Revert "update navbar active"
    This reverts commit a706be49c7ee01d5f72feaacd2ff38f72b1790b2.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -13,10 +13,10 @@
       </div>
       <ul class="nav-header">
         <!-- <li class="nav-item active"><a href="{{ route('beranda') }}">Beranda</a></li> -->
-        <li class="nav-item {{ request()->routeIs('user.beranda.index') ? 'active' : '' }}"><a href="{{ route('user.beranda.index') }}">Beranda</a></li>
-        <li class="nav-item {{ request()->routeIs('user.project.index') ? 'active' : '' }}"><a href="{{ route('user.project.index') }}">Project</a></li>
-        <li class="nav-item {{ request()->routeIs('user.berita.index') ? 'active' : '' }}"><a href="{{ route('user.berita.index') }}">Berita</a></li>
-        <li class="nav-item {{ request()->routeIs('kontak') ? 'active' : '' }}"><a href="{{ route('kontak') }}">Kontak</a></li>
+        <li class="nav-item"><a href="{{ route('user.beranda.index') }}">Beranda</a></li>
+        <li class="nav-item"><a href="{{ route('user.project.index') }}">Project</a></li>
+        <li class="nav-item"><a href="{{ route('user.berita.index') }}">Berita</a></li>
+        <li class="nav-item"><a href="{{ route('kontak') }}">Kontak</a></li>
       </ul>
       <a href="" class="nav-header-responsive">&#9776;</a>
     </nav>
