c2c0b73c23c3f0d4897eac01d9d019f0577161ee
mzaedan <zaedanm108@gmail.com>
2024-06-14 13:26:47 +0700

    update card project

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Carbon\Carbon;
 
 class BerandaController extends Controller
 {
     public function index()
     {
         $berandaItems = Project::all();
+        
+        foreach ($berandaItems as $item) {
+            $item->tanggal = Carbon::parse($item->tanggal)->format('d-m-Y');
+        }
+
         return view('pages.beranda', compact('berandaItems'));
     }
 }
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Carbon\Carbon;
 
 class ProjectController extends Controller
 {
     public function index()
     {
         $projectItems = Project::all();
+
+        foreach ($projectItems as $item) {
+            $item->tanggal = Carbon::parse($item->tanggal)->format('d-m-Y');
+        }
+        
         return view('pages.project', compact('projectItems'));
     }
 
     public function show($slug)
     {
-        $projectItem = Project::where('slug', $slug)->firstOrfail();
+        $projectItem = Project::where('slug', $slug)->firstOrFail();
+
+        $projectItem->tanggal = Carbon::parse($projectItem->tanggal)->format('d-m-Y');
+
         return view('pages.project-detail', compact('projectItem'));
     }
+
 }
